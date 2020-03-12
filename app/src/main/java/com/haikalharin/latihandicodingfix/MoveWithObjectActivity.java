fb6a17cbafe647a33992229b661f8911ee1f416f
=abhu66 <ahmad.dudayef@mitracomm.com>
2020-03-12 17:02:58 +0700

    Initial commit

---
+++ app/src/main/java/com/haikalharin/latihandicodingfix/MoveWithObjectActivity.java
@@ -0,0 +1,22 @@
+package com.haikalharin.latihandicodingfix;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MoveWithObjectActivity extends AppCompatActivity {
+
+    public static final String EXTRA_PERSON = "extra_person";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_move_with_object);
+
+        TextView tvObject = findViewById(R.id.btn_move_activity_object);
+
+        Person person = getIntent().getParcelableExtra(EXTRA_PERSON);
+        String text = "Name : " + person.getName() + ",\nEmail : " + person.getEmail() + ",\nAge : " + person.getAge() + ",\nLocation : " + person.getCity();
+        tvObject.setText(text);
+    }
+}
